0d4e1bfcea92aad22f464d221e54ed1fadf31a28
Lear_1030 <126213720+1030Tim@users.noreply.github.com>
2024-05-03 07:04:03 +0800

    交錯自串.py
    新手解

---
+++ 交錯自串.py
@@ -0,0 +1,27 @@
+n = int(input())#input
+count = 10000
+arr = [ ]
+for i in range(n):
+    k = list(map(str,input()))
+    k_sort = k[:]
+    
+    k_sort.sort()
+    
+    c = 0
+    t = k_sort[0]
+    for j in range(len(k)):
+        if ( t != k_sort[j] ):
+            c+=1
+            t = k_sort[j]
+    #loopend        
+    if (c<count):
+        count = c
+        arr = k
+    elif (c == count):
+        if (arr>k):
+            count = c
+            arr = k
+#loopend
+for i in range(len(arr)):#output
+    print(arr[i],end="")
+print()
