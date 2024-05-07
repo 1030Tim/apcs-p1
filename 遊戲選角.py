a6f491e5bf71fb250a15754bdf56f0245bac4268
Lear_1030 <126213720+1030Tim@users.noreply.github.com>
2024-05-07 19:36:16 +0800

    VUP G.3

---
+++ 遊戲選角.py
@@ -0,0 +1,24 @@
+n = int(input())
+acc = [ ]
+arr = [ ]
+for  i in range(n):
+    a,b = [int(_) for _ in input().split()]
+
+    k = a**2+b**2
+    c = [a,b]
+    acc.append(k)
+    arr.append(c)
+
+n_max = max(acc)
+n_min = min(acc)
+indax_arr  = acc[:]
+indax_arr.sort()
+indax_arr = indax_arr[-2]
+count = n_min
+indax = 0
+for i in range(n):
+    if acc[i] == indax_arr:
+        indax = i
+        break
+            
+print(arr[indax][0],arr[indax][1])
