c9d1a99307c3a0fbbaf8fa2aeb3fb210ba2fbf09
Lear_1030 <126213720+1030Tim@users.noreply.github.com>
2024-05-13 20:03:04 +0800

    新手解

---
+++ 購買力.py
@@ -0,0 +1,9 @@
+n,d = [int(_)for _ in input().split()]
+count = 0
+b = 0
+for i in range(n):
+    k = list(map(int,input().split()))
+    if max(k)-min(k)>=d:
+        count+=1
+        b+=sum(k)//3
+print(count,b)
